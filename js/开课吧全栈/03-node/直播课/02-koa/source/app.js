9653a960b730f49d58553e3aee83f58d3cad3993
王子鑫 <wzix758511990@qq.com>
2020-10-09 08:24:22 +0800

    node-koa2

--- js/开课吧全栈/03-node/直播课/02-koa/source/app.js
+++ js/开课吧全栈/03-node/直播课/02-koa/source/app.js
@@ -1,9 +1,34 @@
 const KKB = require('./kkb')
 const app = new KKB()
+const Router = require('./router')
+const router = new Router();
 
-app.use(ctx => {
-    ctx.body = 'hello ctx' // koa2 中 直接返回ctx.body的内容, 故而kkb.js中最后返回的是body
-})
+const static = require('./static')
+app.use(static(__dirname + '/public'));
+
+router.get('/index', async ctx => { ctx.body = 'index page'; });
+router.get('/post', async ctx => { ctx.body = 'post page'; });
+router.get('/list', async ctx => { ctx.body = 'list page'; });
+router.post('/index', async ctx => { ctx.body = 'post page'; });
+// 路由实例输出⽗中间件 router.routes()
+app.use(router.routes());
+
+
+// const delay = () => new Promise(resolve => setTimeout(() => resolve(), 2000));
+// app.use(async (ctx, next) => {
+//     ctx.body = "1";
+//     await next();
+//     ctx.body += "5";
+// });
+// app.use(async (ctx, next) => {
+//     ctx.body += "2";
+//     await delay();
+//     await next();
+//     ctx.body += "4";
+// });
+// app.use(async (ctx, next) => {
+//     ctx.body += "3";
+// });
 
 app.listen(3000, () => {
     console.log('server running on ' + 3000)
--- js/开课吧全栈/03-node/直播课/02-koa/source/kkb.js
+++ js/开课吧全栈/03-node/直播课/02-koa/source/kkb.js
@@ -4,20 +4,32 @@ const request = require('./request')
 const response = require('./response')
 
 class KKB {
+    // 初始化中间件
+    constructor() {
+        this.middlewares = []
+    }
     listen(...args) {
-        const server = http.createServer((req, res) => {
+        const server = http.createServer(async(req, res) => {
             // this.callback(req, res)
             // 创建上下文
             const ctx = this.createContext(req, res)
-            this.callback(ctx)
+            // this.callback(ctx)
+            // 中间件组合
+            const fn = this.compose(this.middlewares)
+            // 执行组合后的中间件
+            await fn(ctx)
             // 响应
             res.end(ctx.body)
         })
         server.listen(...args)
     }
 
-    use(callback) {
-        this.callback = callback
+    // use(callback) {
+    //     this.callback = callback
+    // }
+
+    use(middleware) {
+        this.middlewares.push(middleware)
     }
 
     /**
@@ -31,6 +43,30 @@ class KKB {
         ctx.req = ctx.request.req = req
         ctx.res = ctx.request.res = res
         return ctx
+    }
+
+    /**
+     * 中间件组合
+     */
+    compose(middlewares) {
+        return function (ctx) { // 上下文
+            return dispatch(0) // 返回最外层
+            function dispatch(i) { // i为middlewares下标
+                let fn = middlewares[i]
+                if (!fn) {
+                    // 不存在 返回一个空承诺
+                    return Promise.resolve()
+                }
+                // 存在则返回fn()执行结果
+                return Promise.resolve(
+                    fn(ctx, function next() {
+                        // promise完成后再执行下一个
+                        // 返回下一个执行承诺
+                        return dispatch(i + 1)
+                    })
+                )
+            }
+        }
     }
 }
 
