bd301f4bf9ce25e5932c712b3655a8716d397a7b
王子鑫 <wzix758511990@qq.com>
2020-11-03 08:27:22 +0800

    11.3

---
+++ js/开课吧全栈/02-react/直播课/03-react-redux & react-router/react-learn/src/k-react-router/BrowserRouter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BrowserRouter extends Component {
+    render() {
+        return (
+            <div>
+                browser router
+            </div>
+        )
+    }
+}
