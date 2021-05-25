4845088e3b9e6468508aa82df0d32f187ed00c61
王子鑫 <wzix758511990@qq.com>
2021-05-25 21:10:17 +0800

    fix: 5.25

--- 其他/LeetCode算法/calcu/2020.8 - now/5.25/不用加号的加法.js
+++ 其他/LeetCode算法/calcu/2020.8 - now/5.25/不用加号的加法.js
@@ -6,10 +6,10 @@
 var add = function (a, b) {
   let sum = 0, carry = 0
   while (b !== 0) {
-    sum = a ^ b
-    carry = (a & b) << 1
-    a = sum
-    b = carry
+    sum = a ^ b // 异或计算未进位的部分
+    carry = (a & b) << 1 // 进位部分
+    a = sum // 保存未进位部分，再次计算
+    b = carry // 保存进位部分，再次计算
   }
   return a
 };
