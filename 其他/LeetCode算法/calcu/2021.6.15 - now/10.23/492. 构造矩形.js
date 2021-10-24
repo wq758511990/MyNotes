0f2f814f30b1c282ca63be8f3ce380a71d17789f
王子鑫 <wzix758511990@qq.com>
2021-10-24 22:23:23 +0800

    feat: 10.23 LC

---
+++ 其他/LeetCode算法/calcu/2021.6.15 - now/10.23/492. 构造矩形.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+const isInteger = (number) => {
+  return number % 1 === 0
+}
+
+var constructRectangle = function (area) {
+  let maxW = Math.floor(Math.sqrt(area))
+  for (let i = maxW; i > 0; i--) {
+    const length = area / i
+    if (isInteger(length)) {
+      return [length, i]
+    }
+  }
+  return [area, 1]
+};
+
+
+console.log(constructRectangle(5)) *
