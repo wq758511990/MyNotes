930eababab94cddf36fdbbb40f627589af00064e
wq758511990 <wzix758511990@qq.com>
2022-06-14 21:57:10 +0800

    feat: 6.14LC

---
+++ 其他/LeetCode算法/calcu/2021.6.15 - now/6.14/498. 对角线遍历.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+var findDiagonalOrder = function (mat) {
+  let m = mat.length;
+  n = mat[0].length;
+  const direction = [
+    [-1, 1],
+    [1, -1],
+  ];
+  const ans = [];
+  let changeTime = 0;
+  let i = 0,
+    j = 0;
+  while (i < m || j < n) {
+    ans.push(mat[i][j]);
+    if (i === m - 1 && j === n - 1) {
+      return ans;
+    }
+    let curDirection = changeTime % 2;
+    i += direction[curDirection][0];
+    j += direction[curDirection][1];
+    if (i < 0 && j > n - 1) {
+      i = 1;
+      j = n - 1;
+      changeTime++;
+    } else if (i > m - 1 && j < 0) {
+      i = m - 1;
+      j = 1;
+      changeTime++;
+    } else if (i < 0) {
+      i = 0;
+      changeTime++;
+    } else if (i > m - 1) {
+      i = m - 1;
+      j += 2;
+      changeTime++;
+    } else if (j < 0) {
+      j = 0;
+      changeTime++;
+    } else if (j > n - 1) {
+      j = n - 1;
+      i += 2;
+      changeTime++;
+    }
+  }
+};
+
+console.log(
+  findDiagonalOrder([
+    [1],
+  ])
+);
