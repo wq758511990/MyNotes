e62d3c4d5786053b3067a359da0ca8563354540b
wq758511990 <wzix758511990@qq.com>
2022-06-18 21:56:17 +0800

    feat: 6.18LC

--- 其他/LeetCode算法/calcu/2022.6.15 - now/6.17/1089. 复写零.js
+++ 其他/LeetCode算法/calcu/2022.6.15 - now/6.17/1089. 复写零.js
@@ -3,14 +3,38 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function (arr) {
-    for(let i = 0; i < arr.length;i++) {
-        if (arr[i] === 0) {
-            arr.splice(i, 0, 0)
-            arr.pop()
-            i++
-        }
+  // for(let i = 0; i < arr.length;i++) {
+  //     if (arr[i] === 0) {
+  //         arr.splice(i, 0, 0)
+  //         arr.pop()
+  //         i++
+  //     }
+  // }
+  // console.log('arr', arr)
+
+  const n = arr.length;
+  let i = -1,
+    top = 0;
+  while (top < n) {
+    i++;
+    if (arr[i] !== 0) top++;
+    else top += 2;
+  }
+  let j = n - 1;
+  if (top === n + 1) {
+    arr[j] = 0;
+    j--;
+    i--;
+  }
+  while (j >= 0) {
+    arr[j] = arr[i];
+    j--;
+    if (arr[i] === 0) {
+      arr[j] = arr[i];
+      j--;
     }
-    console.log('arr', arr)
+    i--;
+  }
 };
 
 console.log(duplicateZeros([1, 2, 3]));
---
+++ 其他/LeetCode算法/calcu/2022.6.15 - now/6.18/剑指 Offer II 029. 排序的循环链表.js
@@ -0,0 +1,29 @@
+
+var insert = function(head, insertVal) {
+    const node = new Node(insertVal);
+    if (!head) {
+        node.next = node;
+        return node;
+    }
+    if (head.next === head) {
+        head.next = node;
+        node.next = head;
+        return head;
+    }
+    let curr = head, next = head.next;
+    while (next !== head) {
+        if (insertVal >= curr.val && insertVal <= next.val) {
+            break;
+        }
+        if (curr.val > next.val) {
+            if (insertVal > curr.val || insertVal < next.val) {
+                break;
+            }
+        }
+        curr = curr.next;
+        next = next.next;
+    }
+    curr.next = node;
+    node.next = next;
+    return head;
+};
