e41e1984c909e7202be698edab8198bf5a2c3a79
wq758511990 <wzix758511990@qq.com>
2022-06-22 20:28:08 +0800

    feat: 6.22LC

---
+++ 其他/LeetCode算法/calcu/2022.6.15 - now/6.22/513. 找树左下角的值.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function findBottomLeftValue(root: TreeNode | null): number {
+    let roots = [root];
+    let ret = 0
+    while(roots.length) {
+        const p = roots.shift()
+        if (p.right) {
+            roots.push(p.right)
+        }
+        if (p.left) {
+            roots.push(p.left)
+        }
+        ret = p.val
+    }
+    return ret
+}
