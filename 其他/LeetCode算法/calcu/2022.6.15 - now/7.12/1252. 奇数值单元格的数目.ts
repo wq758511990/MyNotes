46cd977c853af4a4712d1783f5e6f99660231d38
wq758511990 <wzix758511990@qq.com>
2022-07-12 22:08:21 +0800

    feat: 7.12LC

---
+++ 其他/LeetCode算法/calcu/2022.6.15 - now/7.12/1252. 奇数值单元格的数目.ts
@@ -0,0 +1,60 @@
+function oddCells(m: number, n: number, indices: number[][]): number {
+  let ans = 0;
+  const matrix = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (const indicy of indices) {
+    const ri = indicy[0];
+    const ci = indicy[1];
+    let r = 0,
+      l = 0;
+    while (l < n) {
+      matrix[ri][l] += 1;
+      if (matrix[ri][l] & 1) {
+        ans++;
+      } else {
+        ans--;
+      }
+      l++;
+    }
+    while (r < m) {
+      matrix[r][ci] += 1;
+      if (matrix[r][ci] & 1) {
+        ans++;
+      } else {
+        ans--;
+      }
+      r++;
+    }
+  }
+  return ans;
+}
+
+function oddCells1(m: number, n: number, indices: number[][]): number {
+  let ans = 0;
+  const matrix = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (const indicy of indices) {
+    for (let i = 0; i < n; i++) {
+      matrix[indicy[0]][i]++;
+      if (matrix[indicy[0]][i] & 1) {
+        ans++;
+      } else {
+        ans--;
+      }
+    }
+    for (let i = 0; i < m; i++) {
+      matrix[i][indicy[1]]++;
+      if (matrix[i][indicy[1]] & 1) {
+        ans++;
+      } else {
+        ans--;
+      }
+    }
+  }
+  return ans;
+}
+
+console.log(
+    oddCells1(2, 2, [
+    [1, 1],
+    [0, 0],
+  ])
+);
