efe9d5ddeedd9f28bcc296c160c95012d8abf49b
wq758511990 <wzix758511990@qq.com>
2023-02-09 20:03:13 +0800

    feat: 2.9LC

---
+++ 其他/LeetCode算法/calcu/2022.6.15 - now/2.9/1797. 设计一个验证系统.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number} timeToLive
+ */
+var AuthenticationManager = function (timeToLive) {
+  this.timeToLive = timeToLive;
+  this.authMap = new Map();
+};
+
+/**
+ * @param {string} tokenId
+ * @param {number} currentTime
+ * @return {void}
+ */
+AuthenticationManager.prototype.generate = function (tokenId, currentTime) {
+  this.authMap.set(tokenId, currentTime + this.timeToLive);
+};
+
+/**
+ * @param {string} tokenId
+ * @param {number} currentTime
+ * @return {void}
+ */
+AuthenticationManager.prototype.renew = function (tokenId, currentTime) {
+  const preActiveTime = this.authMap.get(tokenId);
+  if (!preActiveTime || preActiveTime <= currentTime) return;
+  this.authMap.set(tokenId, currentTime + this.timeToLive);
+};
+
+/**
+ * @param {number} currentTime
+ * @return {number}
+ */
+AuthenticationManager.prototype.countUnexpiredTokens = function (currentTime) {
+  for (const [k, v] of this.authMap) {
+    if (v <= currentTime) {
+      this.authMap.delete(k);
+    }
+  }
+  return this.authMap.size;
+};
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * var obj = new AuthenticationManager(timeToLive)
+ * obj.generate(tokenId,currentTime)
+ * obj.renew(tokenId,currentTime)
+ * var param_3 = obj.countUnexpiredTokens(currentTime)
+ */
